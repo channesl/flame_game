180809180d2fffc3b5abcf829d0f7892f21a5db8
channesl <hannesxlindstrom@gmail.com>
2024-10-28 03:05:04 +0100

    exported
    no exe

--- flame_game/scripts/main_menu.gd
+++ flame_game/scripts/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	DisplayServer.window_set_title("Living Flame")
 	$CanvasLayer/MarginContainer/VBoxContainer/MarginContainer/CenterContainer/VBoxContainer/Play_button.grab_focus()
 
 
